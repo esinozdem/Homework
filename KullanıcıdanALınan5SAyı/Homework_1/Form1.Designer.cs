8b5e698b358f8cd0e5f2c27f47ac31b46f37c6be
Süreyya Esin Özdem <sureyyaesin.ozdem@bilgeadamboost.com>
2021-11-08 09:36:57 +0300

    Projenin sunucuya yüklenmesi

---
+++ KullanıcıdanALınan5SAyı/Homework_1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Homework_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(48, 47);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(215, 20);
+            this.txtSayi1.TabIndex = 0;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(12, 91);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(116, 41);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sayı :";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(284, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 238);
+            this.listBox1.TabIndex = 7;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(143, 91);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(120, 41);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "Sayı Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 283);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtSayi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ KullanıcıdanALınan5SAyı/Homework_1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_1
+{
+    public partial class Form1 : Form
+    {
+        //Kulanıcıdan İstenilen 5 Sayının Karesinin Toplamını Hesaplama
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Lütfen Sayı Ekleyiniz!");
+        }
+        
+       int toplam=0;
+
+        
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            
+            foreach (var item in listBox1.Items)
+            {
+                toplam += Convert.ToInt32(item) * Convert.ToInt32(item);
+            }
+            listBox1.Items.Add("Girdiğiniz Sayıların Karesinin Toplamı:" +toplam.ToString());
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(Convert.ToInt32(txtSayi1.Text));
+            if (listBox1.Items.Count > 5)
+            {
+                MessageBox.Show("Sadece 5 sayı Girebilirsiniz.");
+            }
+        }
+       
+
+    }
+
+    
+}
+
