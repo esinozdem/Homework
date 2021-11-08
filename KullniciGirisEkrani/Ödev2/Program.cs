9c70bca685cb6aaa697bd901193e3067f4770c65
Süreyya Esin Özdem <sureyyaesin.ozdem@bilgeadamboost.com>
2021-11-08 09:53:03 +0300

    Projenin Sunucuya Yüklenmesi

---
+++ KullniciGirisEkrani/Ödev2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "";
+            string password = "";
+            do
+            {
+                Console.Write("Lütfen Kullanıcı Adınızı Giriniz:");
+                username = Console.ReadLine();
+                Console.Write("Lüten Şifreyi Giriniz:");
+                password = Console.ReadLine();
+                Console.Clear();
+            }
+            while (username != "admin" || password != "123");
+            
+                Console.WriteLine("Kullanıcı Adı ve Şifreniz Doğru:..");
+                Console.ReadKey();
+            
+
+
+        }
+    }
+}
